bff743506a4dd63cb5956642807d828efa9efd84
Sree Aleti <sree@aletilabs.com>
2017-02-20 20:53:36 +0000

    update the configs

--- tmux_session.sh
+++ tmux_session.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 BASE=$(dirname "$0")
 SNAME=cookbook
+cd $BASE;bundle
 tmux new-session -d -s $SNAME -n vi "cd $BASE;vim"
-tmux new-window -t $SNAME:2 -n server
-tmux split-window -v
+tmux new-window -t $SNAME:2 -n server "cd $BASE;rails s"
+tmux split-window -v "cd $BASE;rails c"
 tmux split-window -h "mysql -h $COOKBOOK_HOST -u $COOKBOOK_USER -p$COOKBOOK_PASSWORD $COOKBOOK_DATABASE"
-tmux new-window -t $SNAME:3 -n bundle "cd $BASE;bundle"
-tmux new-window -t $SNAME:4 -n git
+tmux new-window -t $SNAME:3 -n git
 
 tmux select-window -t $SNAME:1
 tmux attach -t $SNAME
